ec38d1c38c0cfd4f2bdafc9c2098f59fbf2a1c11
Mathias Ledesma <romanmathias21@gmail.com>
2023-12-06 00:30:25 -0300

    contact links was added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
 		<footer className="flex flex-col lg:flex-row gap-4 justify-center py-5">
 			<p className="lg:text-2xl text-xl w-full lg:w-max text-center my-auto">© {t("Developed by")} Mathias Ledesma</p>
 			<div className="flex flex-row justify-center gap-5">
-			<a href="" className="invert" >
+			<a href="https://github.com/Mathi21as/" className="invert" >
 				<Image width={"30"} height={"30"} src={"/resources/redes/github.png"} alt="github"/>
 			</a>
-			<a href="" className="invert">
+			<a href="https://www.linkedin.com/in/mathiled/" className="invert">
 				<Image width={"30"} height={"30"} src={"/resources/redes/linkedin.png"} alt="linkedin"/>
 			</a>
 			</div>
