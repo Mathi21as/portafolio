ae378870f69e2241188a7ce9b28b3af38021e864
Mathias Ledesma <romanmathias21@gmail.com>
2024-09-18 08:25:57 -0300

    Change render menu

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,13 +1,27 @@
+"use client"
+import { useEffect, useState } from "react"
 import NavDesktop from "./NavDesktop"
 import NavMobile from "./NavMobile"
 
 const Nav = ({setLanguage}:any) => {
+	const [windowWidth, setWindowWidth] = useState<number | undefined>();
+
+	useEffect(()=>{
+		if(typeof window != "undefined"){
+			setWindowWidth(window.innerWidth);
+			console.log(window.innerWidth);
+			
+		}
+	}, []);
+
 	return(
 		<>
-		<NavDesktop setLanguage={setLanguage}/>
-		<NavMobile setLanguage={setLanguage}/>
+			{
+				windowWidth && windowWidth >= 1024 ? <NavDesktop setLanguage={setLanguage}/> :
+				<NavMobile setLanguage={setLanguage}/>
+			}
 		</>
 	)
 }
 
-export default Nav
+export default Nav;
